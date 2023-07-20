e280546dea6a370456ce5ca9d664257e58c07a64
daniel-web-developer <daniel_caixeta@protonmail.com>
2023-07-20 13:14:49 +0200

    IT WORKS

--- app/page.js
+++ app/page.js
@@ -1,11 +1,23 @@
+'use client'
 import Image from 'next/image'
+
+const url = "https://api.adviceslip.com/advice"
+
+export async function getSortedPostsData() {
+  const res = await fetch(url, {
+    cache: "no-cache"
+  });
+  const data = await res.json();
+  console.log(data.slip.id);
+  console.log(data.slip.advice);
+}
 
 export default function Home() {
   return (
     <main>
       <div className='flex flex-justcont-c flex-alignit-c home'>
         <div className='home-block'>
-
+          <button onClick={() => getSortedPostsData()}>test</button>
         </div>
       </div>
     </main>
